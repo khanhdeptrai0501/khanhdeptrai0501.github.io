b1320fa99e7878c79699aeb7b2eb19e764226e19
khanhdeptrai0501 <152356816+khanhdeptrai0501@users.noreply.github.com>
2024-08-10 06:45:47 +0700

    Add files via upload

---
+++ AutoWin.lua
@@ -0,0 +1,61 @@
+--®WndyOc
+--©Vip Home Channel
+
+  gg.toast(os.date('🔸️sᴄʀɪᴘ ᴍᴏᴅ ᴘᴇs ᴍᴏʙɪʟᴇ🔸️'))
+  function OS1()
+    MN1 = gg.multiChoice({
+     '🔹️☞ Bật ᴡɪɴ🔹️vip',
+     '🔹️☞ Tắt ᴡɪɴ🔹️vip',
+        '🚫 ᎬxᎥᏆ 🚫'
+    }, nil, 'sᴄʀɪᴘ ᴍᴏᴅ ᴘᴇs ᴍᴏʙɪʟᴇ\n 🟡 ву ѕσ̛и вι̣ρ νи 🟡')
+    if MN1 == nil then
+    else
+      if MN1[1] == true then
+        wh1()
+      end
+      if MN1[2] == true then
+        wh2()
+       end
+     if MN1[3] == true then
+        OS2()
+     end
+     if MN1[4] == true then
+        OS2()
+      end
+      end
+     XGCK = -1
+end
+     function wh1()
+gg.clearResults()
+gg.setSpeed(0.01)
+gg.toast('🔹️Vui lòng chờ ᴀuтo wιɴ🔹️')
+end
+
+function wh2()
+ gg.clearResults()
+gg.setSpeed(1.0)
+gg.toast('🔹️☞ ᴀuтo wιɴ🔹️ ✔️')
+ end
+
+  function OS2()
+    gg.toast('🔸️мoᴅ ʙʏ sơɴ ʙι̣ᴘ🔸️')
+    print('Cholie')
+    os.exit()
+  end
+  cs = 'Cholie'
+  while true do
+    if gg.isVisible(true) then
+      XGCK = 1
+      gg.setVisible(false)
+    end
+    if XGCK == 1 then
+      OS1()
+    end
+    function OS2()
+    gg.toast('🔸️мoᴅ ʙʏ sơɴ ʙι̣ᴘ🔸️')
+    print('Cholie')
+    os.exit()
+   end
+  end
+--®WndyOc
+--©Vip Home Channel
